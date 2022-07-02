5d3072dd55961a96a6eba0850fabf0c4df4d73f3
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2022-07-02 19:10:41 +0300

    i18n

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   publicPath: '/',
   pluginOptions: {
-    cordovaPath: 'src-cordova'
+    cordovaPath: 'src-cordova',
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false
+    },
   }
 }
