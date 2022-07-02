e06592799adba4fc16a7031bd99339d2f2d89ed0
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2022-07-03 00:26:29 +0300

    i18n

--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import i18n from './i18n'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
