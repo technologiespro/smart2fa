486a099c7e48ab1725d4f3445c54bc0effa0ef48
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2022-07-03 01:02:16 +0300

    migrationImport

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,38 @@
 <template>
   <div class="home">
 
+    <b-button pill variant="primary">+</b-button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 //import HelloWorld from '@/components/HelloWorld.vue'
+import parser from "otpauth-migration-parser";
 
 export default {
   name: 'Home',
   components: {
 
+  },
+  methods: {
+    async migrationImport(dataUri) {
+      const parsedDataList = await parser(dataUri);
+      for (let otpSecretInfo of parsedDataList) {
+        console.log(otpSecretInfo);
+        /* =>
+          {
+            secret: 'xxxxxxxxxxxxxxxxxxxxxxxxxxx',
+            name: 'sample',
+            issuer: 'sample',
+            algorithm: 'sha1',
+            digits: 6,
+            type: 'totp',
+            counter: Long { low: 0, high: 0, unsigned: false }
+          }
+        */
+      }
+    }
   }
 }
 </script>
