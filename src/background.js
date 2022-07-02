22e659792961976a2b326411c90acc0fbbbb68ff
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2022-07-03 01:03:06 +0300

    createWindow

--- src/background.js
+++ src/background.js
@@ -13,8 +13,8 @@ protocol.registerSchemesAsPrivileged([
 async function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 480,
+    height: 800,
     webPreferences: {
       
       // Use pluginOptions.nodeIntegration, leave this alone
