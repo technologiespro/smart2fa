4ed1cccc3fd3b24303a6a03b516969b8c5081876
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2022-07-03 02:46:50 +0300

    QrScanImport2fa

---
+++ src/components/QrScanImport2fa.vue
@@ -0,0 +1,136 @@
+<template>
+  <div>
+    <qrcode-stream style="width:200px; height:200px;" @decode="onDecode" @init="onInit" />
+  </div>
+</template>
+
+<script>
+import parser from "otpauth-migration-parser";
+
+document.addEventListener('deviceready', function() {
+
+
+  let permissions = cordova.plugins.permissions;
+  permissions.requestPermission(permissions.CAMERA, success, error);
+
+  /*
+  navigator.permissions.query({name: 'camera'})
+      .then((permissionObj) => {
+        console.log(permissionObj.state);
+      })
+      .catch((error) => {
+        console.log('Got error :', error);
+      })
+
+   */
+  /*
+  navigator.getUserMedia (
+      // constraints
+      {
+        video: true,
+        audio: true
+      },
+
+      // successCallback
+      function(localMediaStream) {
+        var video = document.querySelector('video');
+        video.src = window.URL.createObjectURL(localMediaStream);
+        video.onloadedmetadata = function(e) {
+          // Do something with the video here.
+        };
+      },
+
+      // errorCallback
+  );
+
+   */
+
+  var now = Date.now();
+  /*
+  navigator.mediaDevices.getUserMedia({audio: false, video: true})
+      .then(function(stream) {
+        console.log('Got stream, time diff :', Date.now() - now);
+      })
+      .catch(function(err) {
+        console.log('GUM failed with error, time diff: ', Date.now() - now);
+      });
+
+
+   */
+  navigator.mediaDevices.getUserMedia({video: true})
+      .then(function(stream) {
+        document.getElementById('camera').srcObject = stream;
+      }).catch(function() {
+    alert('could not connect stream');
+  });
+
+  function error() {
+    console.warn('Camera permission is not turned on');
+  }
+
+  function success( status ) {
+    if( !status.hasPermission ) error();
+  }
+  //console.log('cordova.plugins.permissions is now available');
+});
+
+import {QrcodeStream} from 'vue-qrcode-reader'
+
+export default {
+  name: "QrScanImport2fa",
+  data() {
+    return {
+      dataUri: "",
+      importResult: [],
+      error: "",
+    }
+  },
+  components: { QrcodeStream },
+  methods: {
+    async migrationImport() {
+      const parsedDataList = await parser(this.dataUri);
+      this.importResult = [];
+      for (let otpSecretInfo of parsedDataList) {
+        this.importResult.push(otpSecretInfo);
+        /* =>
+          {
+            secret: 'xxxxxxxxxxxxxxxxxxxxxxxxxxx',
+            name: 'sample',
+            issuer: 'sample',
+            algorithm: 'sha1',
+            digits: 6,
+            type: 'totp',
+            counter: Long { low: 0, high: 0, unsigned: false }
+          }
+        */
+      }
+    },
+    onDecode (result) {
+      this.dataUri = result
+    },
+    async onInit (promise) {
+      try {
+        await promise
+      } catch (error) {
+        if (error.name === 'NotAllowedError') {
+          this.error = "ERROR: you need to grant camera access permisson"
+        } else if (error.name === 'NotFoundError') {
+          this.error = "ERROR: no camera on this device"
+        } else if (error.name === 'NotSupportedError') {
+          this.error = "ERROR: secure context required (HTTPS, localhost)"
+        } else if (error.name === 'NotReadableError') {
+          this.error = "ERROR: is the camera already in use?"
+        } else if (error.name === 'OverconstrainedError') {
+          this.error = "ERROR: installed cameras are not suitable"
+        } else if (error.name === 'StreamApiNotSupportedError') {
+          this.error = "ERROR: Stream API is not supported in this browser"
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
