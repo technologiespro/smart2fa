735f0cbf287783267c17910cfd675ddcf8141f81
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2022-07-03 00:40:03 +0300

    env

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import '@babel/polyfill'
-import 'mutationobserver-shim'
-import Vue from 'vue'
-import './plugins/bootstrap-vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import i18n from './i18n'
+import '@babel/polyfill';
+import 'mutationobserver-shim';
+import Vue from 'vue';
+import './plugins/bootstrap-vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import i18n from './i18n';
 
 Vue.config.productionTip = false
 
