96063b805d916639a4b99d341acf85d62529d67c
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2022-07-03 03:47:35 +0300

    storage main

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,50 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import VuexPersistence from 'vuex-persist'
+import * as localforage from 'localforage';
+import {pullAll, keys} from 'lodash';
+
+import vuexPersistReady from '@/store/plugins/vuex-persist-ready'
+
+import keys2fa from '@/store/modules/keys2fa'
+
+
+//import * as memoryDriver from 'localforage-driver-memory'
 
 Vue.use(Vuex)
 
+const modules = {
+  keys2fa: keys2fa,
+};
+
+// Modules that should not be persisted
+const ignoreModules = [];
+
+// localforage.defineDriver(memoryDriver)
+// localforage.setDriver([localforage.LOCALSTORAGE, localforage.INDEXEDDB, localforage.WEBSQL, memoryDriver._driver])
+
+const vuexPersist = new VuexPersistence({
+  // It is necessary to enable the strict mode to watch to mutations, such as `RESTORE_MUTATION`
+  strictMode: true,
+  asyncStorage: true,
+  key: 'sth2fa',
+  storage: localforage,
+  modules: pullAll(keys(modules), ignoreModules)
+});
+
 export default new Vuex.Store({
-  state: {
-  },
+  modules,
+  strict: process.env.NODE_ENV !== 'production',
   mutations: {
+    RESTORE_MUTATION: vuexPersist.RESTORE_MUTATION
   },
   actions: {
+    resetData() {
+      return localforage.clear()
+    }
   },
-  modules: {
-  }
+  plugins: [
+    vuexPersist.plugin,
+    vuexPersistReady,
+  ]
 })
