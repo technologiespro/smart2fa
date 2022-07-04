d81d64803d11de1d58176aa0773d5e1e02f5165b
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2022-07-04 13:33:26 +0300

    app

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,35 @@
 <template>
   <div id="app">
+<!--
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <span class="badge badge-info">{{allKeys.length}}</span> <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
     </div>
+-->
     <router-view/>
   </div>
 </template>
 
 <script>
-
+import {generateToken} from "node-2fa";
 export default {
+  computed: {
+    allKeys() {
+
+      let keys = this.$store.getters['keys2fa/faKeys'];
+      let result = [];
+      for (let i=0; i < keys.length; i++) {
+        result[i] = {
+          issuer: keys[i].issuer,
+          name: keys[i].name,
+          token: generateToken(keys[i].secret).token
+        }
+      }
+
+      return (result)
+    }
+
+  },
   async created() {
     this.$store._vm.$on('vuex-persist:ready', async () => {
      //console.log(await this.$store.getters['keys2fa/faKeys'])
