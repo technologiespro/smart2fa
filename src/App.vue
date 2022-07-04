afc16f25f1f7038af9877894d9a740dac22d57da
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2022-07-04 18:20:22 +0300

    nav prepare

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,38 @@
 <template>
   <div id="app">
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="info" fixed>
+        <b-navbar-brand href="#">Smart2FA</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+              <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+              <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+            </b-nav-form>
+
+            <b-nav-item-dropdown text="Lang" right>
+              <b-dropdown-item href="#">RU</b-dropdown-item>
+              <b-dropdown-item href="#">CN</b-dropdown-item>
+              <b-dropdown-item href="#">EN</b-dropdown-item>
+            </b-nav-item-dropdown>
+
+            <b-nav-item-dropdown right>
+              <!-- Using 'button-content' slot -->
+              <template #button-content>
+                <em>User</em>
+              </template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
 <!--
     <div id="nav">
       <span class="badge badge-info">{{allKeys.length}}</span> <router-link to="/">Home</router-link>
@@ -7,6 +40,9 @@
     </div>
 -->
     <router-view/>
+    <p class="text-center">
+      <a target="_blank" href="https://smartholdem.io" class="small">(C) SmartHoldem</a>
+    </p>
   </div>
 </template>
 
@@ -39,24 +75,20 @@ export default {
 }
 </script>
 <style>
+body, html {
+  padding: 0;
+  margin: 0;
+  width:100%;
+  overflow-x: hidden;
+}
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  width: 100% !important;
+  height: 100vh;
 }
 </style>
