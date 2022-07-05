9404741f48bda87d19375130f83efb3c5c809724
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2022-07-05 15:05:13 +0300

    makeToast

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,167 +1,172 @@
 <template>
-  <div class="home overflow-hidden">
+  <div class="w-100">
+    <div v-if="op === 'importKeys'" style="backdrop-filter: blur(4px); background: rgba(43,68,87,0.63); width: 100%; height: 100vh; position: absolute; top:0; padding: 4% !important; margin: 0 !important; ">
+      <div style="margin-left: auto; margin-right: auto;width:25px;margin-bottom: 5px;">
+        <b-button @click="op = 'home'" :pill="true" variant="info">X</b-button>
+      </div>
+
+
+      <QrScanImport2fa style="margin-left: auto; margin-right: auto; width:250px; height:250px; border: solid 2px #333;"/>
+    </div>
     <div class="row">
 
-
-      <div class="ml-auto mr-auto">
-        <div v-if="op === 'importKeys'">
-          <p>Импорт ключейн из Google Auth</p>
-          <p>
-            <QrScanImport2fa/>
-          </p>
+      <div class="container mb-2" style="margin-top:55px;">
+        <div class="w-100" style="background: rgb(193 51 162);position: fixed; margin-top: 0px">
+          <!--<b-progress :value="((countdown * (seconds / totalSeconds * 100) / 100))" :max="countdown" animated></b-progress>-->
+          <span style="float: right;height: 5px;background: #1fab1a;"
+                :style="'width:' + ((screenWidth * (seconds / totalSeconds * 100) / 100)) + 'px;'"></span>
         </div>
+      <div v-for="(item, idx) in allKeys" v-bind:key="item.secret" class="w-100 pl-2 item-2fa">
+        {{idx+1}} <span v-show="!item.name.includes(item.issuer)">{{ item.issuer }}</span> {{ item.name }}<br/>
+        <span class="font-weight-bold token">
+          {{item.token}}
+        </span>
       </div>
-
-
-    </div>
-
-    <div class="row overflow-hidden">
-      <div class="container-fluid" style="background: rgb(193 51 162);;">
-        <b-progress :value="((countdown * (seconds / totalSeconds * 100) / 100))" :max="countdown" animated></b-progress>
-        <span style="float: right;width:40px;height: 5px;background: #1fab1a;" :style="'width:' + ((countdown * (seconds / totalSeconds * 100) / 100)) + 'px;'"></span>
       </div>
 
     </div>
 
 
-    <div class="mt-1 text-left ml-2" style="height:640px; overflow-y:auto">
-
-      <div v-for="item in allKeys" v-bind:key="item.secret">
-        <span v-show="!item.name.includes(item.issuer)">{{item.issuer}}</span> {{ item.name }}<br/>
-        <span class="font-weight-bolder text-info">{{ item.token }}</span>
-        <hr/>
-      </div>
-    </div>
-    <p class="small">Создано в SmartHoldem</p>
-    <div class="container-fluid">
-      <span class="badge badge-info float-left">{{allKeys.length}}</span>
-      <b-button-group>
-        <b-button @click="op = 'addKey'">Добавить</b-button>
+    <div class="container">
+      <div class="ml-auto mr-auto">
+        <b-button class="mr-1" @click="op = 'addKey'">Добавить</b-button>
         <b-button @click="op = 'importKeys'">Импортировать</b-button>
-      </b-button-group>
+      </div>
+
     </div>
+
+
   </div>
 </template>
 
 <script>
 import QrScanImport2fa from '@/components/QrScanImport2fa.vue'
-const twofactor = require("node-2fa");
-
+import {generateToken} from "node-2fa";
+//import AnimatedNumber from "animated-number-vue";
 
 export default {
   name: 'Home',
-  components: {QrScanImport2fa},
+  components: {
+    QrScanImport2fa,
+    //AnimatedNumber
+  },
   data() {
     return {
-      op: null,
+      op: 'home',
       importResult: [],
       allKeys: [],
       timer: null,
       process: false,
       seconds: 0,
-      totalSeconds: 40,
+      totalSeconds: 32,
       timerTimeout: null,
-      countdown: 480,
+      screenWidth: 480,
     }
   },
-  /*
   computed: {
-    allKeys() {
-
+    storedKeys() {
+      return this.$store.getters['keys2fa/faKeys'];
+      /*
       let keys = this.$store.getters['keys2fa/faKeys'];
       let result = [];
       for (let i=0; i < keys.length; i++) {
         result[i] = {
+          secret: keys[i].secret,
           issuer: keys[i].issuer,
           name: keys[i].name,
-          token: twofactor.generateToken(keys[i].secret).token
+          token: generateToken(keys[i].secret).token
         }
       }
-
       return (result)
+       */
     }
-
   },
-   */
-methods: {
-  async regenerateTokens() {
-    let keys = this.allKeys;
-    let result = [];
-    for (let i=0; i < keys.length; i++) {
-      result[i] = {
-        secret: keys[i].secret,
-        issuer: keys[i].issuer,
-        name: keys[i].name,
-        token: twofactor.generateToken(keys[i].secret).token
-      }
-    }
-    this.allKeys = result;
-    await this.generateTokens();
-  },
-
-    async listTokens() {
-      this.$store._vm.$on('vuex-persist:ready', async () => {
-        let keys = this.$store.getters['keys2fa/faKeys'];
-        //console.log(keys)
-        let result = [];
-        for (let i=0; i < keys.length; i++) {
-          result[i] = {
-            secret: keys[i].secret,
-            issuer: keys[i].issuer,
-            name: keys[i].name,
-            token: twofactor.generateToken(keys[i].secret).token
-          }
-        }
-        this.allKeys = result;
-        await this.generateTokens();
+  methods: {
+    makeToast(variant = null, title= 'Success', message = '') {
+      this.$bvToast.toast(message, {
+        title: title,
+        toaster: 'b-toaster-bottom-right',
+        variant: variant,
+        solid: true
       })
+    },
+    async regenerateTokens() {
+      this.allKeys = [];
+      for (let i = 0; i < this.storedKeys.length; i++) {
+        this.allKeys[i] = {
+          secret: this.storedKeys[i].secret,
+          issuer: this.storedKeys[i].issuer,
+          name: this.storedKeys[i].name,
+          token: generateToken(this.storedKeys[i].secret).token
+        }
+      }
+      //this.allKeys = result;
+      //await this.generateTokens();
     },
 
     async generateTokens() {
-      //clearInterval(this.timer);
+      console.log('generateTokens');
+      clearInterval(this.timer);
       clearTimeout(this.timerTimeout);
+      await this.regenerateTokens();
+
       this.seconds = this.totalSeconds;
 
       this.timer = setInterval(async () => {
         this.seconds = this.seconds - 1;
+        if (this.seconds === 0) {
+          this.seconds = this.totalSeconds;
+          await this.generateTokens();
+        }
       }, 1000);
 
-
-      this.timerTimeout = setTimeout(async() => {
+      /*
+      this.timerTimeout = setTimeout(async () => {
         this.seconds = this.totalSeconds;
-        await this.regenerateTokens();
+        await this.generateTokens();
         clearInterval(this.timer);
-        }, this.seconds * 1000);
-    }
+      }, this.totalSeconds * 1000);
+       */
+
+    },
+
   },
   async created() {
-    await this.$eventBus.on('qr:importKeys', async (data) => {
-      for (let i = 0; i < data.length; i++) {
-        let isDublicate = false;
-        for (let j = 0; j < this.importResult.length; j++) {
-            if (this.importResult[j].secret === data[i].secret) {
-              isDublicate = true;
-            }
-        }
-        if (!isDublicate) {
-          this.importResult.push(data[i]);
-        }
-      }
-      await this.$store.dispatch('keys2fa/setKeys', this.importResult);
-      await this.listTokens();
+    this.screenWidth = window.innerWidth;
+    this.$store._vm.$on('vuex-persist:ready', async () => {
+      await this.generateTokens();
     });
-    await this.listTokens();
+    await this.$eventBus.on('qr:importKeys', async (data) => {
+      this.op = 'home';
+      await this.$store.dispatch('keys2fa/setKeys', data);
+      await this.generateTokens();
+      await this.makeToast('success', 'Ключи успешно добавлены ' + data.length);
+    });
   }
 }
 </script>
 
 <style>
-.home {
-  max-width: 100%;
-  height: 768px;
-  /* border: solid 1px #ccc;*/
-  margin-left: auto;
-  margin-right: auto;
+.item-2fa {
+  border-bottom: dashed 1px #76797c;
+  padding-bottom: 4px;
+  padding-top: 2px;
+}
+
+.item-2fa:hover {
+  background: #bdc0c2;
+  cursor: pointer;
+  border-bottom: 1px;
+  border-bottom: solid 1px #1371ce;
+}
+
+.token {
+  font-size: 20px;
+  color: #233b5d;
+  text-shadow: 1px 1px 0.06em rgba(226, 246, 97, 0.6);
+  /*
+  animation: color-change 32s infinite;
+   */
 }
 
 svg {
@@ -169,9 +174,11 @@ svg {
   margin: 15px auto;
 }
 
-#loader
-{ fill: #0088cc }
+#loader {
+  fill: #0088cc
+}
 
-#border
-{ fill: #00517a }
+#border {
+  fill: #00517a
+}
 </style>
