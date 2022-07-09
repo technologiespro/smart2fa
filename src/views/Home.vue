28eb82aa1f9eddd69e812a547b7061eed1f3410b
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2022-07-09 06:50:36 +0300

    btnBotAdd

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="w-100">
     <b-navbar toggleable="lg" type="dark" variant="info" :sticky="true" fixed="top"
               style="position: fixed;background: linear-gradient(-45deg, #38096d 1%, #4eabf7 48%, #00cccf);">
-      <b-navbar-brand><img width="22px" src="images/logo48.png"/> Smart2FA</b-navbar-brand>
+      <b-navbar-brand><img width="22px" src="images/logo48.png"/> Smart2FA <span class="badge badge-dark">{{seconds}}</span></b-navbar-brand>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
 
@@ -42,7 +42,7 @@
       <div style="margin-left: auto; margin-right: auto;width:25px;margin-bottom: 5px; margin-top: 10px;">
         <b-button @click="op = 'home'" :pill="true" variant="info">X</b-button>
       </div>
-      <p class="text-white">{{$t('google_migrate_info')}}</p>
+      <p class="text-white">{{ $t('google_migrate_info') }}</p>
     </div>
 
     <div v-if="op === 'addKeyIn'"
@@ -51,7 +51,7 @@
       <div style="margin-left: auto; margin-right: auto;width:25px;margin-bottom: 5px; margin-top: 10px;">
         <b-button @click="op = 'home'" :pill="true" variant="info">X</b-button>
       </div>
-      <p class="text-white">{{$t('token_add_info_input')}}</p>
+      <p class="text-white">{{ $t('token_add_info_input') }}</p>
     </div>
 
     <div v-if="op === 'addKeyQR'"
@@ -61,7 +61,7 @@
       <div style="margin-left: auto; margin-right: auto;width:25px;margin-bottom: 5px; margin-top: 10px;">
         <b-button @click="op = 'home'" :pill="true" variant="info">X</b-button>
       </div>
-      <p class="text-white">{{$t('token_add_info_qr')}}</p>
+      <p class="text-white">{{ $t('token_add_info_qr') }}</p>
     </div>
 
     <div v-if="op === 'itemRawData'"
@@ -78,52 +78,65 @@
     </div>
 
     <div class="row">
-      <div class="container mb-2" style="margin-top:55px;padding-bottom: 38px;">
+      <div class="container" style="margin-top:55px;padding-bottom: 48px;position: relative;">
         <div class="w-100" style="background: rgb(193 51 162);position: fixed; margin-top: 0px">
-          <!--<b-progress :value="((countdown * (seconds / totalSeconds * 100) / 100))" :max="countdown" animated></b-progress>-->
+          <!--
+          <b-progress :value="((screenWidth * (seconds / totalSeconds * 100) / 100))" :max="screenWidth" animated style="border-radius:0;z-index:1000000;"></b-progress>
+          -->
           <span style="float: right;height: 5px;background: #1fab1a;"
                 :style="'width:' + ((screenWidth * (seconds / totalSeconds * 100) / 100)) + 'px;'"></span>
         </div>
 
         <!-- ITEMS -->
-        <div v-for="(item, idx) in allKeys" v-bind:key="item.secret" class="w-100 pl-2 item-2fa"
-             style="position: relative"
-             @click="itemSelect(idx)" v-bind:class="{ itemActive: idx === selectedItem }">
-          <div v-show="submenu === idx" class="itemSubMenu">
-            <span @click="itemDel(idx)" class="badge badge-danger mr-3">DEL</span>
-            <span @click="itemRawData(idx)" class="badge badge-info mr-3">RAW</span>
-            <span @click="submenu = null" class="badge badge-warning text-uppercase">{{ $t('cancel') }}</span>
-          </div>
-          <span v-show="!item.name.includes(item.issuer)">{{ item.issuer }}</span> {{ item.name }}<br/>
-          <div @click="submenu !== idx ? submenu = idx : submenu = null" class="float-right pr-2">
-            <b-icon icon="three-dots-vertical" font-scale="1"></b-icon>
-          </div>
-          <span class="font-weight-bold token">
+        <div style="margin-top:10px;">
+          <div v-for="(item, idx) in allKeys" v-bind:key="item.secret" class="w-100 pl-2 item-2fa"
+               style="position: relative"
+               @click="itemSelect(idx)" v-bind:class="{ itemActive: idx === selectedItem }">
+            <div v-show="submenu === idx" class="itemSubMenu">
+              <span @click="itemDel(idx)" class="badge badge-danger mr-3">DEL</span>
+              <span @click="itemRawData(idx)" class="badge badge-info mr-3">RAW</span>
+              <span @click="submenu = null" class="badge badge-warning text-uppercase">{{ $t('cancel') }}</span>
+            </div>
+
+            <span class="item-name" v-show="!item.name.includes(item.issuer)">{{ item.issuer }}</span> <span
+              class="item-name">{{ item.name }}</span>
+
+
+            <br/>
+
+            <div @click="submenu !== idx ? submenu = idx : submenu = null" class="float-right pr-2">
+              <b-icon icon="three-dots-vertical" font-scale="1"></b-icon>
+            </div>
+            <span class="font-weight-bolder token">
             {{ item.token }}
           </span>
+          </div>
         </div>
+
         <!-- ITEMS END -->
 
-        <img @click="ddShow = !ddShow" class="btnBotAdd" src="images/add.png"/>
-        <div v-show="ddShow"
-             style="border-radius: 6px 6px 0 6px; position: absolute; bottom: 56px;right: 15px; background: linear-gradient(-45deg, #9510b2 1%, #4eabf7 48%, #fbbe8d);">
-          <b-nav vertical class="w-100">
-            <b-nav-item @click="op = 'addKeyQR'; ddShow = false"><span class="text-white">{{$t('qr_scan')}}</span>
-            </b-nav-item>
-            <b-nav-item @click="op = 'addKeyIn'; ddShow = false"><span class="text-white">{{$t('enter_key')}}</span>
-            </b-nav-item>
-          </b-nav>
-        </div>
 
 
       </div>
 
     </div>
 
+    <img @click="ddShow = !ddShow" class="btnBotAdd" src="images/add.png"/>
+    <div v-show="ddShow"
+         style="border-radius: 6px 6px 0 6px; position: absolute; bottom: 56px;right: 15px; background: linear-gradient(-45deg, #9510b2 1%, #4eabf7 48%, #fbbe8d);">
+      <b-nav vertical class="w-100">
+        <b-nav-item @click="op = 'addKeyQR'; ddShow = false"><span class="text-white">{{ $t('qr_scan') }}</span>
+        </b-nav-item>
+        <b-nav-item @click="op = 'addKeyIn'; ddShow = false"><span class="text-white">{{ $t('enter_key') }}</span>
+        </b-nav-item>
+      </b-nav>
+    </div>
+
+
     <div v-show="ddShow" class="text-left w-100 pl-3"
          style="position: absolute; bottom: 0px; font-size: 16pt;background: rgba(255,255,255,0.89);">
-      <a target="_blank" href="https://smartholdem.io" class="small text-danger">
-        {{ $t('powered') }} <img width="18px" src="images/48x48.png"/> SmartHoldem v{{ this.currentVersion() }}
+      <a target="_blank" href="https://smartholdem.io" class="small text-dark">
+        {{ $t('powered') }} <img width="18px" src="images/48x48.png"/> SmartHoldem v{{ currentVersion() }}
       </a>
     </div>
   </div>
@@ -279,6 +292,10 @@ export default {
 </script>
 
 <style>
+.item-name {
+  font-weight: normal;
+}
+
 .btnBotAdd {
   position: absolute;
   bottom: 10px;
@@ -312,13 +329,13 @@ export default {
 }
 
 .item-2fa:active {
-  background: linear-gradient(-45deg, #fbbe8d 1%, #4eabf7 48%, #fbbe8d);
+  /*background: linear-gradient(-45deg, #fbbe8d 1%, #4eabf7 48%, #fbbe8d);*/
   animation: blinker 1s linear;
 }
 
 .itemActive {
-  text-shadow: 1px 1px 0.06em rgba(246, 244, 97, 0.6);
-  background: linear-gradient(-45deg, #fbbe8d 1%, #4eabf7 48%, #fbbe8d) !important;
+  /* text-shadow: 1px 1px 0.06em rgba(246, 244, 97, 0.6);*/
+  background: linear-gradient(-45deg,  #9aebef 48%, #e5f0fe) !important;
   border-bottom: 1px;
   border-bottom: solid 1px #1371ce;
 }
@@ -337,9 +354,9 @@ export default {
 }
 
 .token {
-  font-size: 20px;
+  font-size: 18pt;
   color: #233b5d;
-  text-shadow: 1px 1px 0.06em rgba(226, 246, 97, 0.6);
+  /*text-shadow: 1px 1px 0.06em rgba(226, 246, 97, 0.6);*/
   /*
   animation: color-change 32s infinite;
    */
