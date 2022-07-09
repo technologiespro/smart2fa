064145287db1ab6d48a0c802261b1536578eee31
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2022-07-10 01:23:09 +0300

    enter_secret_key

--- src/components/AddNewKeyIn.vue
+++ src/components/AddNewKeyIn.vue
@@ -2,7 +2,7 @@
   <div>
     <b-card bg-variant="light">
       <b-form-input v-model="name" :placeholder="$t('account_name')"></b-form-input>
-      <b-form-input class="mt-2" v-model="secret" placeholder="Укажите ключ"></b-form-input>
+      <b-form-input class="mt-2" v-model="secret" :placeholder="$t('enter_secret_key')"></b-form-input>
 
       <div class="mt-2">
         <b-button class="w-100" @click="AddKeyToApp" variant="outline-primary">{{$t('btn_add')}}</b-button>
