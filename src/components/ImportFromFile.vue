abd229e50b9c000dc30a9e9e9604783882be6f7e
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2022-07-10 14:19:49 +0300

    file reader reader

--- src/components/ImportFromFile.vue
+++ src/components/ImportFromFile.vue
@@ -2,22 +2,42 @@
   <div>
     <b-form-file
         v-model="file"
+        :multiple="true"
         :state="Boolean(file)"
         placeholder="Choose a file or drop it here..."
         drop-placeholder="Drop file here..."
+        @input="loadData"
+        accept=".json"
     ></b-form-file>
-    <div class="mt-3">Selected file: {{ file ? file.name : '' }}</div>
+    <div class="mt-3">{{ fileValue }}</div>
+
 
 
   </div>
 </template>
 
 <script>
+import CryptoJS from "crypto-js";
 export default {
   name: "ImportFromFile",
   data() {
     return {
       file: null,
+      fileValue: '',
+      step: 0,
+    }
+  },
+  methods: {
+    async loadData() {
+      if (this.file) {
+        let reader = new FileReader();
+        reader.readAsText(this.file[0]);
+        let _self = this
+        reader.onload = async function() {
+          _self.fileValue = reader.result;
+        };
+      }
+
     }
   }
 }
