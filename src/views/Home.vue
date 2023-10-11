9277853441811e42111285dc3b20579b1cfb2590
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2023-10-12 01:21:44 +0300

    home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="w-100">
     <b-navbar toggleable="lg" type="dark" variant="info" :sticky="true" fixed="top"
               style="position: fixed;background: linear-gradient(-45deg, #146eb9 1%, #146eb9 48%, #146eb9);">
-      <b-navbar-brand><img width="24px" src="images/logo48.png"/> Smart2FA
+      <b-navbar-brand><img width="24px" src="images/logo48.png"/> SMART 2FA
         <span class="badge badge-success"
               style="border: solid 1px rgba(35,59,93,0.76);">{{ (seconds < 10 ? '0' + seconds : seconds) }}</span>
       </b-navbar-brand>
