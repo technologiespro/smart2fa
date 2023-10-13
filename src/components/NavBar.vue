9eac3e71320df8ba50e0034f4bb372237929c3d6
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2023-10-14 02:29:22 +0300

    default navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="info" :sticky="true" fixed="top"
+            style="position: fixed;background: linear-gradient(-45deg, #146eb9 1%, #146eb9 48%, #146eb9);">
+    <b-navbar-brand><img width="24px" src="images/logo48.png"/> SMART 2FA
+      <span class="badge badge-success"
+            style="border: solid 1px rgba(35,59,93,0.76);">
+
+        </span>
+    </b-navbar-brand>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+}
+</script>
+
+<style scoped>
+
+</style>
