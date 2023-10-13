3ddafe470dedbb49cc3494313a7d0d73d90cbfcd
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2023-10-14 02:11:35 +0300

    router

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/set-pin',
+    name: 'SetPin',
+    component: () => import(/* webpackChunkName: "SetPin" */ '../views/SetPin.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "SetPin" */ '../views/Login.vue')
   }
 ]
 
