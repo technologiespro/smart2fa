6cec4bef2f1b73b1969718ff9d9166c8ee159838
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2023-10-14 05:21:21 +0300

    unlock page

---
+++ src/views/Unlock.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <NavBar/>
+    <div class="overflow-hidden mt-auto mb-auto pt-5">
+      <p class="mt-5 text-center">Unlock app</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar';
+
+export default {
+  name: "Login",
+  components: {
+    NavBar
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
